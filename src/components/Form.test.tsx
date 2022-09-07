c9ec8f47c58a3739b55b10f2094587bc5964fed7
Jeff <jefferson.santos@maxmilhas.com.br>
2022-09-07 18:09:42 -0300

    test: create test to add a participant if there is a name filled in

--- src/components/Form.test.tsx
+++ src/components/Form.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Form from "./Form";
 
 test("When input is empty, new participants cannot be added.", () => {
@@ -14,4 +14,25 @@ test("When input is empty, new participants cannot be added.", () => {
   expect(input).toBeInTheDocument();
 
   expect(button).toBeDisabled();
+});
+
+test("Add a participant if there is a name filled in.", () => {
+  render(<Form />);
+
+  const input = screen.getByPlaceholderText(
+    "Insira os nomes dos participantes"
+  );
+
+  const button = screen.getByRole("button");
+
+  fireEvent.change(input, {
+    target: {
+      value: "Ana Catarina",
+    },
+  });
+
+  fireEvent.click(button);
+
+  expect(input).toHaveFocus();
+  expect(input).toHaveValue("");
 });
