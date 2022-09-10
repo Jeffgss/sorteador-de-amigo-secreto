cac3fb50b1fffc1ea82ae6623b0d244355d800d9
Jeff <jefferson.santos@maxmilhas.com.br>
2022-09-10 13:42:15 -0300

    feat: add timeout to disappear error message after 5 seconds

--- src/components/state/hook/useAddParticipant.ts
+++ src/components/state/hook/useAddParticipant.ts
@@ -9,6 +9,9 @@ export const useAddParticipant = () => {
   return (participantName: string) => {
     if (list.includes(participantName)) {
       setError("Nomes duplicados não são permitidos!");
+      setTimeout(() => {
+        setError("");
+      }, 5000);
       return;
     }
     return setList((currentList) => [...currentList, participantName]);
