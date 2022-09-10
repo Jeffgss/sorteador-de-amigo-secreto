c133fec77391f7b0f0039a4abf7400f86f08caf8
Jeff <jefferson.santos@maxmilhas.com.br>
2022-09-10 13:42:47 -0300

    test: create test to check if the error message goes away after 5 seconds

--- src/components/Form.test.tsx
+++ src/components/Form.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { act, fireEvent, render, screen } from "@testing-library/react";
 import Form from "./Form";
 import { RecoilRoot } from "recoil";
 
@@ -77,4 +77,47 @@ test("Duplicate names cannot be added to the list.", () => {
 
   const errorMessage = screen.getByRole("alert");
   expect(errorMessage.textContent).toBe("Nomes duplicados não são permitidos!");
+});
+
+test("The error message should go away after the timers.", () => {
+  jest.useFakeTimers();
+
+  render(
+    <RecoilRoot>
+      <Form />
+    </RecoilRoot>
+  );
+
+  const input = screen.getByPlaceholderText(
+    "Insira os nomes dos participantes"
+  );
+
+  const button = screen.getByRole("button");
+
+  fireEvent.change(input, {
+    target: {
+      value: "Ana Catarina",
+    },
+  });
+
+  fireEvent.click(button);
+
+  fireEvent.change(input, {
+    target: {
+      value: "Ana Catarina",
+    },
+  });
+
+  fireEvent.click(button);
+
+  let errorMessage = screen.queryByRole("alert");
+  expect(errorMessage).toBeInTheDocument();
+
+  act(() => {
+    jest.runAllTimers();
+  });
+
+  errorMessage = screen.queryByRole("alert");
+
+  expect(errorMessage).toBeNull();
 });
