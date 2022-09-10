dd230a74b4e704029947922c013e13dc6d2088af
Jeff <jefferson.santos@maxmilhas.com.br>
2022-09-10 17:05:15 -0300

    test: add mock values to tests

--- src/components/ParticipantsList.test.tsx
+++ src/components/ParticipantsList.test.tsx
@@ -2,8 +2,19 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import { RecoilRoot } from "recoil";
 import { ParticipantList } from "./ParticipantList";
+import { useParticipantsList } from "./state/hook/useParticipantsList";
+
+jest.mock("./state/hook/useParticipantsList", () => {
+  return {
+    useParticipantsList: jest.fn(),
+  };
+});
 
 describe("An empty list of participants.", () => {
+  beforeEach(() => {
+    (useParticipantsList as jest.Mock).mockReturnValue([]);
+  });
+
   test("Must be rendered without elements.", () => {
     render(
       <RecoilRoot>
@@ -18,6 +29,9 @@ describe("An empty list of participants.", () => {
 
 describe("A filled list of participants.", () => {
   const participants = ["Ana", "Catarina"];
+  beforeEach(() => {
+    (useParticipantsList as jest.Mock).mockReturnValue(participants);
+  });
 
   test("Must be rendered without elements.", () => {
     render(
