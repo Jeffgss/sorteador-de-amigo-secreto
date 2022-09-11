089a5e16b3d97418207e8388e4a7f8d41f4cb744
Jeff <jefferson.santos@maxmilhas.com.br>
2022-09-11 18:29:57 -0300

    feat: create config page

---
+++ src/pages/Config.tsx
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer";
+import Form from "../components/Form";
+import { ParticipantList } from "../components/ParticipantList";
+
+const Config = () => {
+  return (
+    <>
+      <Form />
+      <ParticipantList />
+      <Footer />
+    </>
+  );
+};
+
+export default Config;
