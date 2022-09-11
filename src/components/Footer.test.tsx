03af0a44df8a5844f14dcbfca2a7a8c1a35d730e
Jeff <jefferson.santos@maxmilhas.com.br>
2022-09-11 14:12:09 -0300

    test: test footer with a mock list

--- src/components/Footer.test.tsx
+++ src/components/Footer.test.tsx
@@ -2,8 +2,27 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import { RecoilRoot } from "recoil";
 import Footer from "./Footer";
+import { useParticipantsList } from "./state/hook/useParticipantsList";
+
+jest.mock("./state/hook/useParticipantsList", () => {
+  return {
+    useParticipantsList: jest.fn(),
+  };
+});
+
+const mockNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => {
+  return {
+    useNavigate: mockNavigate,
+  };
+});
 
 describe("Where there are not enough participants.", () => {
+  beforeEach(() => {
+    (useParticipantsList as jest.Mock).mockReturnValue([]);
+  });
+
   test("The joke cannot be started.", () => {
     render(
       <RecoilRoot>
@@ -14,5 +33,24 @@ describe("Where there are not enough participants.", () => {
     const button = screen.getByRole("button");
 
     expect(button).toBeDisabled();
+  });
+});
+
+describe("When there are enough participants.", () => {
+  const participants = ["Ana", "Catarina", "Maria"];
+  beforeEach(() => {
+    (useParticipantsList as jest.Mock).mockReturnValue(participants);
+  });
+
+  test("The joke can be started.", () => {
+    render(
+      <RecoilRoot>
+        <Footer />
+      </RecoilRoot>
+    );
+
+    const button = screen.getByRole("button");
+
+    expect(button).not.toBeDisabled();
   });
 });
